886932ea0f5ea5d1e7452b2c7141eb0797facc7f
xpl <rocket.mind@gmail.com>
2017-06-30 18:47:17 +0300

    tabs → spaces

--- string.bullet.js
+++ string.bullet.js
@@ -5,9 +5,9 @@ const { ansiEscapeCodes, printableCharacters } = require ('printable-characters'
 module.exports = (bullet, arg) => {
 
                     const isArray = Array.isArray (arg),
-                    	  lines   = isArray ? arg : arg.split ('\n'),
-                    	  indent  = bullet.replace (ansiEscapeCodes, '').replace (printableCharacters, ' '),
-                    	  result  = lines.map ((line, i) => (i === 0) ? (bullet + line) : (indent + line))
+                          lines   = isArray ? arg : arg.split ('\n'),
+                          indent  = bullet.replace (ansiEscapeCodes, '').replace (printableCharacters, ' '),
+                          result  = lines.map ((line, i) => (i === 0) ? (bullet + line) : (indent + line))
                     
                     return isArray ? result : result.join ('\n')
                 }
