dce33411cbe4354a152528ba6cff07d1a7197a22
xpl <rocket.mind@gmail.com>
2017-07-21 23:43:23 +0300

    deps update

--- string.bullet.js
+++ string.bullet.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const { ansiEscapeCodes, printableCharacters } = require ('printable-characters')
+const { blank } = require ('printable-characters')
 
 module.exports = (bullet, arg) => {
 
                     const isArray = Array.isArray (arg),
                           lines   = isArray ? arg : arg.split ('\n'),
-                          indent  = bullet.replace (ansiEscapeCodes, '').replace (printableCharacters, ' '),
+                          indent  = blank (bullet),
                           result  = lines.map ((line, i) => (i === 0) ? (bullet + line) : (indent + line))
                     
                     return isArray ? result : result.join ('\n')
