f1b0ee26d7245d3305bf1d894dca448f1e82b443
xpl <rocket.mind@gmail.com>
2017-07-23 09:32:57 +0300

    fully automated build & deploy with TravisCI

--- test.js
+++ test.js
@@ -1,24 +1,11 @@
-var assert = require ('assert')
-var bullet = require ('./string.bullet')
+const assert = require ('assert')
+    , bullet = require ('./string.bullet')
 
 describe ('string.bullet', () => {
 
     it ('works', () => {
 
-        var b = '\t\u001b[101m• \u001b[49m'
-
-        var src = 'foo\n' +
-                  'bar\n' +
-                  'baz'
-
-        var result = '\t\u001b[101m• \u001b[49mfoo\n' +
-                   '\t  bar\n' +
-                   '\t  baz'
-
-        var bulleted = bullet (b, src)
-
-        console.log ('\n', bulleted, '\n')
-
-        assert.equal (bulleted, result)
+        assert.equal     (bullet ('\t\u001b[101m• \u001b[49m', 'foo\nbar\nbaz'),        '\t\u001b[101m• \u001b[49mfoo\n\t  bar\n\t  baz')
+        assert.deepEqual (bullet ('\t\u001b[101m• \u001b[49m', ['foo', 'bar', 'baz']), ['\t\u001b[101m• \u001b[49mfoo', '\t  bar', '\t  baz'])
     })
 })
